fc273ddb6b631086163e0be0c372d0fa6fb95c92
Tirso Pascual <tpascualgarcia@gmail.com>
2022-05-19 14:49:10 +0900

    search by categories in index page and some style

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'mast
 gem 'devise'
 gem 'cloudinary', '~> 1.16.0'
 gem 'pundit'
+gem 'pg_search', '~> 2.3.0'
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,11 +2,20 @@ class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  CATEGORIES = ['Itasha', 'Decotora', 'Bosozoku_Bike']
+
   validates :brand, presence: true
   validates :model, presence: true
-  CATEGORIES = ['Itasha', 'Decotora', 'Bosozoku_Bike']
   validates :category, inclusion: { in: CATEGORIES }
   validates :location, presence: true
   validates :price, presence: true
   validates :description, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_category_and_location,
+    against: [:category],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
